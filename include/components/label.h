442f829b71969126aab5758c52e4cfc541ab7370
Soumyadeep-Chakravarti <soumyadeepsai1@gmail.com>
2025-07-07 16:42:12 +0530

    starting

--- include/components/label.h
+++
@@ -1,8 +0,0 @@
-#ifndef TUI_COMPONENT_LABEL_H
-#define TUI_COMPONENT_LABEL_H
-
-#include "core/component.h"
-
-TUIComponent tui_label_create(const char *text, int x, int y, uint8_t fg, uint8_t bg);
-
-#endif // TUI_COMPONENT_LABEL_H
